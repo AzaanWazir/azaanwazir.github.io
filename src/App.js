642e42e6199d85d17a18b27b2a7d2e8e72df5457
Zereni <58187265+Zereni@users.noreply.github.com>
2023-06-02 01:49:26 -0400

    Landed Page Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import myImage from './images/Hero.jpg';
+import Typewriter from 'typewriter-effect';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Navbar></Navbar>
-      </header>
+      <Navbar></Navbar>
+      <div className='hero'>
+        <img src={myImage}></img>
+        <h1 className='mainText'>Azaan Wazir</h1>
+        <div className='subText'>
+          <span>Systems Design Engineer, University of Waterloo</span>
+        </div>
+        <div className='jobExperience'>
+          <span>
+            <Typewriter
+              options={{
+                strings: ['FullStack Developer @ Tremeng Group', 'Software Engineer @ Alert Driving' , 'Product Lead @ Tulip Retail'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Link } from 'react-router-dom';
 import "./Navbar.css";
 import logo from '../../logo.svg'
 
-function Navbar() {
+const Navbar = () => {
     return (
         <div>
             <nav>
@@ -12,14 +12,10 @@ function Navbar() {
                     <div className='pages'>
                         <Link to="/">Home</Link>
                         <Link to="/projects">Projects</Link>
-                        {/* Should lead to scrolling down to projects section on page */}
                         <Link to="/contact">Contact</Link>
                     </div>
                 </BrowserRouter>
             </nav>
-            <div className='navSpace'>
-                
-            </div>
         </div>
         
     );
