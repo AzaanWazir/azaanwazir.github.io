e6f462d837105541a3e4ac254fc5ed9d0c06b4fe
Zereni <58187265+Zereni@users.noreply.github.com>
2023-07-30 21:37:27 -0400

    Attempting github pages

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Projects.css";
+import Portfolio from './ProjectSnippets/Portfolio';
 
 const Projects = (props) => {
-    const { imgs, srcs } = props;
-    <div className="projects">
-        
-    </div>
+    return (
+        <div className="projects">
+            <h1 className="sectionHeader">Projects</h1>
+            <Portfolio></Portfolio>
+        </div>
+    )
 }
 
 export default Projects
