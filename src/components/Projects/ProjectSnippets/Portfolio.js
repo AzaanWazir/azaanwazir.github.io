cb895b75de604f3785909740d024ed3cf1d5df2d
Zereni <58187265+Zereni@users.noreply.github.com>
2023-07-30 21:37:36 -0400

    Attempting github pages

---
+++ src/components/Projects/ProjectSnippets/Portfolio.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Portfolio.css";
+
+const Projects = (props) => {
+    <div className="portfolio">
+        
+    </div>
+}
+
+export default Projects
---
+++ src/components/Projects/ProjectSnippets/Timekit.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Timekit.css";
+
+const Projects = (props) => {
+    <div className="timekit">
+        
+    </div>
+}
+
+export default Projects
